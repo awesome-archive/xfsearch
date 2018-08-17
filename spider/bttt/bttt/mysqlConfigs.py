cdc9a49a0404610cb432848736662c4612243da2
JackLee <1142742228@qq.com>
2018-08-17 08:59:29 +0800

    2018.8.17.8.59
    spider

---
+++ spider/bttt/bttt/mysqlConfigs.py
@@ -0,0 +1,9 @@
+mysql_config = {
+    'MYSQL_HOSTS': '119.29.34.133',
+    'MYSQL_USER': 'root',
+    'MYSQL_PASSWORD': 'zxc201305',
+    'MYSQL_PORT': 3306,
+    'MYSQL_DB': 'moviedb',
+    'MYSQL_SHEET': 't_movies',
+    'img_path': '/home/unbuntu-lls-one/work_field/project_search/test2/bttt/bttt/spiders/douban.jpg'
+}
