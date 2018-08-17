02064fc285ef0cc29b475d34a65af7f3814a0744
Rar <986771570@qq.com>
2018-08-17 14:49:54 +0800

    update
    防止用户输入html标签或者超长字符串被记录数据库

--- src/main/java/com/xuyuanfeng/utlis/RecordUtils.java
+++ src/main/java/com/xuyuanfeng/utlis/RecordUtils.java
@@ -4,12 +4,22 @@ import java.sql.SQLException;
 import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.swing.text.html.HTML;
 
 public class RecordUtils {
 
 	// **記錄用戶的輸入 用於熱搜》》
 	public static void record(HttpServletRequest req, String text) {
 		String ip = req.getRemoteAddr();
+		//记录的时候应该防止数据量太大的情况。
+		if(text.length()>30)
+		{
+			text=text.substring(0,30);
+		}
+		text=HTMLUtils.delHTMLTag(text);
+        text=HTMLUtils.stripHtml(text);		
+        		
+		
 		try {
 			JDBCUtils.executeInsert("insert into t_searchkey (ip,text,createtime,isDeleted) values(?,?,?,?)", 
 					ip,text, new Date(), false);
