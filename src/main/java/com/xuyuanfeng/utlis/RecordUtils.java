c3f8e7374eb60459a8c67b01a51f5e69ad281f18
Rar <986771570@qq.com>
2018-08-17 15:25:49 +0800

    update
    更新防止用户输入html标签以及长字符串插入数据库报错

--- src/main/java/com/xuyuanfeng/utlis/RecordUtils.java
+++ src/main/java/com/xuyuanfeng/utlis/RecordUtils.java
@@ -10,6 +10,13 @@ public class RecordUtils {
 	// **記錄用戶的輸入 用於熱搜》》
 	public static void record(HttpServletRequest req, String text) {
 		String ip = req.getRemoteAddr();
+		if(text.length()>30)
+		{
+			text=text.substring(0,30);
+		}
+		text=HTMLUtils.delHTMLTag(text);
+		text=HTMLUtils.stripHtml(text);
+		
 		try {
 			JDBCUtils.executeInsert("insert into t_searchkey (ip,text,createtime,isDeleted) values(?,?,?,?)", 
 					ip,text, new Date(), false);
